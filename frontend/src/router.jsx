71005f93ae8071a9fe945304d0ea0b806b9378c2
roka59 <154201123+roka59@users.noreply.github.com>
2024-05-13 19:33:27 +1000

    added react router

---
+++ frontend/src/router.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className="text-center text-8xl m-10">E-commerce</div>,
+    },
+  ]);
+
+  export default router;
